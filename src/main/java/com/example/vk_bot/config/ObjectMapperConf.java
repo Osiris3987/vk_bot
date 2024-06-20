2368d5e7c66ed46b7450d3bab66c9aa6352b35e9
Oleg Lomakin <yfnf72@gmail.com>
2024-06-21 01:19:19 +0300

    fixed object mapper

--- src/main/java/com/example/vk_bot/config/ObjectMapperConf.java
+++ src/main/java/com/example/vk_bot/config/ObjectMapperConf.java
@@ -1,5 +1,7 @@
 package com.example.vk_bot.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class ObjectMapperConf {
     @Bean
     public ObjectMapper mapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
     }
 }
